0003c64ba22dbb86f95c33a328c92c0483b278c9
jxbz <jembernstein@gmail.com>
2024-03-11 20:52:02 -0400

    sweep scripts

--- sweep/sweep.sh
+++ sweep/sweep.sh
@@ -4,6 +4,7 @@ for LR in 0.015625 0.03125 0.0625 0.125 0.25 0.5 1.0 2.0 4.0; do
      export WIDTH=$WIDTH
      export LR=$LR
      export ARCH=resmlp
+     export TRAIN_STEPS=10000
      sbatch --export=ALL sweep/run.sh
 done
 done
