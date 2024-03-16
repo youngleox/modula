987d0514a3045f86a8c8468d5ecc148cb788815b
Jeremy Bernstein <jembernstein@gmail.com>
2024-03-15 20:19:31 -0400

    implemented conv2d

--- module/atomic.py
+++ module/atomic.py
@@ -84,3 +84,43 @@ class Linear(Module):
             self.weight *= 1 - lr * wd
 
         self.weight.grad = None
+
+
+class Conv2d(Module):
+    def __init__(self, out_channels, in_channels, kernel_size, stride=1, padding=0, mass=1):
+        super().__init__()
+        self.mass = mass
+        self.sensitivity = 1
+
+        self.out_channels = out_channels
+        self.in_channels = in_channels
+        self.k = kernel_size
+        self.stride = stride
+        self.pad = padding
+        self.scale = math.sqrt(out_channels / in_channels) / (kernel_size ** 2)
+
+    def forward(self, x):
+        return self.scale * torch.nn.functional.conv2d(x, self.weight, None, self.stride, self.pad)
+
+    def initialize(self, device):
+        self.weight = torch.empty((self.out_channels, self.in_channels, self.k, self.k), device=device, requires_grad=True)
+        for kx in range(self.k):
+            for ky in range(self.k):
+                torch.nn.init.orthogonal_(self.weight[:,:,kx,ky])
+        self.parameters = [self.weight]
+
+        self.momentum = torch.zeros_like(self.weight)
+        self.u = torch.randn_like(self.weight[0])
+
+    @torch.no_grad()
+    def update(self, lr, beta, wd):
+        self.momentum += (1-beta) * (self.weight.grad - self.momentum)
+        norm, self.u.data = spectral_norm(self.momentum, self.u)
+
+        if (norm == 0.0).any():
+            self.u = torch.randn_like(self.weight[0])
+        else:
+            self.weight -= lr * self.momentum / norm
+            self.weight *= 1 - lr * wd
+
+        self.weight.grad = None
