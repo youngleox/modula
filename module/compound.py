d8bd6ad79341ecb20011a17f75f5cce72c38f33c
Jeremy Bernstein <jembernstein@gmail.com>
2024-03-20 18:53:32 -0400

    make resnet variable depth

--- module/compound.py
+++ module/compound.py
@@ -34,26 +34,30 @@ def ResMLP(args, input_dim, output_dim):
 
 def ResCNN(args, input_dim, output_dim):
 	width = args.width
-	block_depth = args.blockdepth
+	blockdepth = args.blockdepth
+	num_blocks = args.depth
 	input_dim = input_dim[0]
+
+	assert args.depth > 1, "need at least two blocks"
+
+	Layer      = lambda width : MeanSubtract() @ Abs() @ Conv2D(width, width)                     @ LayerNorm()
+	DownSample = lambda width : MeanSubtract() @ Abs() @ Conv2D(2*width, width, stride = 2)       @ LayerNorm()
+	Skip       = lambda width : Conv2D(2*width, width, kernel_size = 1, stride = 2, padding = 0)  @ LayerNorm()
 
 	net = Conv2D(width, input_dim)
 
-	residue = (MeanSubtract() @ Abs() @ Conv2D(width, width) @ LayerNorm()) ** block_depth
-	block = 1/2 * Identity() + 1/2 * residue
-	net = block**2 @ net
+	block = 1/2 * Identity() + 1/2 * Layer(width)**blockdepth
+	net = block ** 2 @ net
 
 	for _ in range(3):
-		residue = MeanSubtract() @ Abs() @ Conv2D(2*width, width, stride = 2)  @ LayerNorm()
-		residue = MeanSubtract() @ Abs() @ Conv2D(2*width, 2*width) @ LayerNorm() @ residue
-		skip = Conv2D(2*width, width, kernel_size = 1, stride = 2, padding = 0)  @ LayerNorm()
-		block_1 = 1/2 * skip + 1/2 * residue
-
-		residue = (MeanSubtract() @ Abs() @ Conv2D(2*width, 2*width) @ LayerNorm())**2
-		block_2 = 1/2 * Identity() + 1/2 * residue
-
+		block_1 = 1/2 * Skip(width) + 1/2 * Layer(2*width)**(blockdepth-1) @ DownSample(width)
+		block_2 = 1/2 * Identity()  + 1/2 * Layer(2*width)**blockdepth
 		net = block_2 @ block_1 @ net
+
 		width *= 2
+
+	block = (1-1/num_blocks) * Identity() + 1/num_blocks * Layer(width)**blockdepth
+	net = block ** num_blocks @ net
 
 	final = Linear(output_dim, width) @ Flatten() @ AvgPool()
 	net = final @ net
