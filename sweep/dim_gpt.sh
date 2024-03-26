f1317f0132a8e7f49788b42f9acf963f32e0b4b5
jxbz <jembernstein@gmail.com>
2024-03-25 20:07:06 -0400

    fix bug

--- sweep/dim_gpt.sh
+++ sweep/dim_gpt.sh
@@ -1,6 +1,6 @@
-for WIDTH in 8 16 32 64 128 256 512 1024; do
+for DIM in 8 16 32 64 128 256 512 1024; do
 for LR in 0.015625 0.03125 0.0625 0.125 0.25 0.5 1.0 2.0 4.0; do
-     export TAG=gpt_d_embed_sweep/$WIDTH/$LR
+     export TAG=gpt_d_embed_sweep/$DIM/$LR
      export LOG_INTERVAL=100
      export SEED=0
      export BATCH_SIZE=12
@@ -13,7 +13,7 @@ for LR in 0.015625 0.03125 0.0625 0.125 0.25 0.5 1.0 2.0 4.0; do
      export WIDTH=0
      export CONTEXT=64
      export NUM_HEADS=4
-     export D_EMBED=$WIDTH
+     export D_EMBED=$DIM
      export D_QUERY=32
      export D_VALUE=32
      export OPTIM=mgd
