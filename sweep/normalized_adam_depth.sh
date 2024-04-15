5c494bd501eb0efe0ad1aee46d2b1fe1832b43b2
jxbz <jembernstein@gmail.com>
2024-04-15 14:42:39 -0400

    normalize is arg

--- sweep/normalized_adam_depth.sh
+++ sweep/normalized_adam_depth.sh
@@ -1,6 +1,6 @@
-for DEPTH in 2 4 8 16 32 64 128; do
+for DEPTH in 2 4 8 16 32 64 128 256 512; do
 for LR in 0.015625 0.03125 0.0625 0.125 0.25 0.5 1.0 2.0 4.0; do
-     export TAG=resmlp_depth_sweep/$DEPTH/$LR
+     export TAG=normalized/adam/$DEPTH/$LR
      export LOG_INTERVAL=100
      export SEED=0
      export BATCH_SIZE=128
@@ -16,11 +16,11 @@ for LR in 0.015625 0.03125 0.0625 0.125 0.25 0.5 1.0 2.0 4.0; do
      export D_EMBED=0
      export D_QUERY=0
      export D_VALUE=0
-     export OPTIM=mgd
      export LOSS=xent
      export LR=$LR
-     export BETA=0.9
+     export BETA1=0.9
+     export BETA2=0.99
      export WD=0.01
-     sbatch --export=ALL sweep/run.sh
+     sbatch --export=ALL sweep/run_normalized.sh
 done
 done
